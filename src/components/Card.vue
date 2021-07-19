7625b119346c31635cbdbad8c53bb1e0244b5513
sulamita_althaus <sulamitaalthaus44@gmail.com>
2021-07-19 20:30:32 -0300

    completed form and validations

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,78 +1,111 @@
 <template>
-    <div class="container">
-        <img src="../assets/img/mastercard.png" alt="">
-        <div class="content">
-            <h1 class="numberCard">{{numberCard}}</h1>
-            <h1 class="name">{{name}}</h1>
-            <div class="footer">
-                <label>VALIDADE
-                <h1 class="validity">{{validity}}</h1></label>
-                <label>CVV
-                <h1 class="cvv">{{cvv}}</h1></label>
-            </div>
-        </div>
+  <div class="container">
+    <img :src="getImgUrl()" alt="" />
+    <div class="content">
+      <h1 class="numberCard">{{ numberCard }}</h1>
+      <h1 class="name">{{ name }}</h1>
+      <div class="footer">
+        <label
+          >VALIDADE
+          <h1 class="validity">{{ validity }}</h1></label
+        >
+        <label
+          >CVV
+          <h1 class="cvv">{{ cvv }}</h1></label
+        >
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
+import Mastercard from "../assets/img/mastercard.png";
+import Visa from "../assets/img/visa.png";
+import NoCard from "../assets/img/nocard.png";
+
 export default {
-  name: 'Card',
+  name: "Card",
   props: {
     numberCard: Number,
     name: String,
     validity: String,
-    cvv: Number
-  }
-}
+    cvv: Number,
+  },
+  watch: {
+    numberCard: function () {
+      this.getImgUrl();
+    },
+  },
+  methods: {
+    getImgUrl() {
+      let image = NoCard;
+      let number = String(this.numberCard.split(" ").join(""));
+      let cards = {
+        Visa: /^4[0-9]{6}/,
+        Mastercard: /^5[1-5][0-9]{6}/,
+      };
+      for (var card in cards)
+        if (number.match(cards[card])) {
+          if (card === "Mastercard") {
+            image = Mastercard;
+          } else if (card === "Visa") {
+            image = Visa;
+          }
+        }
+
+      return image;
+    },
+  },
+};
 </script>
 
 <style scoped>
 @font-face {
-    font-family: kredit;
-    src: url(../assets/font/kredit-front.ttf);
+  font-family: kredit;
+  src: url(../assets/font/kredit-front.ttf);
 }
-.container{
-    display: grid;
-    width: 30vw;
-    height: 38vh;
-    background: radial-gradient(#707070, #363636);
-    border-radius: 10px;
-    box-shadow: 1px 5px 10px #C6C6C6;
-    padding: 0 15px;
-    align-items: center;
+.container {
+  display: grid;
+  width: 30vw;
+  height: 38vh;
+  background: radial-gradient(#707070, #363636);
+  border-radius: 10px;
+  box-shadow: 1px 5px 10px #c6c6c6;
+  padding: 0 15px;
+  align-items: center;
 }
-.container img{
-    position: absolute;
-    right: 0;
-    margin: 10px 10px 0 0 ;
-
+.container img {
+  max-width: 15%;
+  position: absolute;
+  right: 0;
+  margin: 10px 10px 0 0;
 }
-.content{
-    display: block;
-    color: #C6C6C6;
-    font-family: kredit;
-    font-size: 16px;
-    text-align: left;
-    line-height: 20px;
-    padding: 0 15px;
+.content {
+  display: block;
+  color: #c6c6c6;
+  font-family: kredit;
+  font-size: 16px;
+  text-align: left;
+  line-height: 20px;
+  padding: 0 15px;
 }
-.name{
-    font-size: 20px;
-    font-weight: normal;
+.name {
+  font-size: 20px;
+  font-weight: normal;
 }
-.footer{
-    display: flex;
-    justify-items: left;
-    justify-content: space-between;
-    width: 50%;
-    line-height: 15px;
-    font-size: 15px;
+.footer {
+  display: flex;
+  justify-items: left;
+  justify-content: space-between;
+  width: 50%;
+  line-height: 15px;
+  font-size: 15px;
 }
-.footer label{
-    font-weight: 800;
+.footer label {
+  font-weight: 800;
 }
-.footer h1{
-    font-size: 15px;
-    font-weight: normal;
+.footer h1 {
+  font-size: 15px;
+  font-weight: normal;
 }
 </style>
--- src/views/Payment.vue
+++ src/views/Payment.vue
@@ -1,81 +1,180 @@
 <template>
-    <div class="container">
-        <Header/>
-        <div class="card">
-          <Card :numberCard="numberCard ? numberCard : '5555 5555 5555 5555'" 
-          :name="name ? name : 'Walter J. White'" 
-          :validity="validity ? validity : '12/26'" 
-          :cvv="cvv ? cvv : '123'" />
-        </div>
-        <div class="content">
-          <label for="">Número do cartão</label>
-          <input type="text" 
-          v-model="numberCard" 
-          v-mask="'#### #### #### ####'">
-
-          <label for="">Nome</label>
-          <input type="text" v-model="name">
-
-          <div class="cardInformation">
-            <label>Validade (MM/AA)
-              <input type="text" 
-              v-model="validity"
-              v-mask="'##/##'">
-            </label>
-
-            <label>CVV
-              <input type="text" 
-              v-model="cvv"
-              v-mask="'###'">
-            </label>
-
-            <label>CPF
-              <input type="text" 
-              v-model="cpf"
-              v-mask="'###.###.###-##'">
-            </label>
-          </div>
-
-          <label for="">Número de parcelas</label>
-          <input type="text" v-model="installment">
-        </div>
-        <Back msg="Alterar forma de pagamento"/>
+  <div class="container">
+    <Header />
+    <div class="card">
+      <Card
+        :numberCard="numberCard ? numberCard : '5555 5555 5555 5555'"
+        :name="name ? name : 'Walter J. White'"
+        :validity="validity ? validity : '12/26'"
+        :cvv="cvv ? cvv : '123'"
+      />
     </div>
+
+    <div class="content">
+      <label for="">Número do cartão</label>
+      <input
+        id="numberCard"
+        v-model="numberCard"
+        v-mask="'#### #### #### ####'"
+        :style="{
+          borderBottom: validation && !numberCard ? '1px solid #FF0000' : null,
+        }"
+      />
+      <span v-if="validationNumberCard || (validation && !numberCard)"
+        >Número do cartão inválido</span
+      >
+
+      <label for="">Nome</label>
+      <input
+        id="name"
+        v-model="name"
+        :style="{
+          borderBottom: validation && !name ? '1px solid #FF0000' : null,
+        }"
+      />
+      <span v-if="validation && !name">Insira seu nome completo</span>
+
+      <div class="cardInformation">
+        <label
+          >Validade (MM/AA)
+          <input
+            id="validity"
+            v-model="validity"
+            v-mask="'##/##'"
+            :style="{
+              borderBottom:
+                validation && !validity ? '1px solid #FF0000' : null,
+            }"
+          />
+          <span v-if="validation && !validity">Data Inválida</span>
+        </label>
+
+        <label
+          >CVV
+          <img
+            src="../assets/img/information.svg"
+            title="O código de segurança do cartão"
+          />
+          <input
+            id="cvv"
+            v-model="cvv"
+            v-mask="'###'"
+            :style="{
+              borderBottom: validation && !cvv ? '1px solid #FF0000' : null,
+            }"
+          />
+          <span v-if="validation && !cvv">CVV não informado</span>
+        </label>
+
+        <label
+          >CPF
+          <input
+            id="cpf"
+            v-model="cpf"
+            v-mask="'###.###.###-##'"
+            :style="{
+              borderBottom: validation && !cpf ? '1px solid #FF0000' : null,
+            }"
+          />
+          <span v-if="validation && !cpf">CPF inválido</span>
+        </label>
+      </div>
+
+      <label for="">Número de parcelas</label>
+      <select
+        id="installment"
+        v-model="installment"
+        :style="{
+          borderBottom: validation && !installment ? '1px solid #FF0000' : null,
+        }"
+      >
+        <option :value="1">1x R$1.198,80 (à vista)</option>
+        <option :value="2">2x R$599,40</option>
+        <option :value="3">3x R$399,60</option>
+        <option :value="4">4x R$299,70</option>
+        <option :value="5">5x R$239,76</option>
+      </select>
+      <span v-if="validation && !installment"
+        >Escolha o número de parcelas</span
+      >
+
+      <button @click="setPayment">Pagar</button>
+    </div>
+    <Back msg="Alterar forma de pagamento" />
+  </div>
 </template>
 
 <script>
-import Header from '../components/Header'
-import Card from '../components/Card'
-import Back from '../components/Back'
+import Header from "../components/Header";
+import Card from "../components/Card";
+import Back from "../components/Back";
+
 export default {
-  name: 'Payment',
+  name: "Payment",
   components: {
     Header,
     Card,
-    Back
+    Back,
   },
-  data(){
-    return {
-      numberCard: "",
-      name: "",
-      validity: "",
-      cvv: "",
-      cpf: "",
-      installment: ""
-    }
-  }
-}
+  data: () => ({
+    numberCard: "",
+    name: "",
+    validity: "",
+    cvv: "",
+    cpf: "",
+    installment: "",
+    validation: false,
+    validationNumberCard: false,
+  }),
+  methods: {
+    async setPayment() {
+      this.validation = true;
+      console.log(this.installment)
+      if (this.numberCard.split(" ").join("").length < 16) {
+        this.validationNumberCard = true;
+        return;
+      }
+      if (
+        !this.numberCard ||
+        !this.name ||
+        !this.validity ||
+        !this.cvv ||
+        !this.cpf ||
+        !this.installment
+      ) {
+        return;
+      } else {
+        this.validationNumberCard = false;
+        //   await api
+        //     .post(`/newsolicitation`, {
+        //       numberCard: this.numberCard,
+        //       name: this.name,
+        //       validity: this.validity,
+        //       cvv: this.cvv,
+        //       cpf: this.cpf,
+        //       installment: this.installment,
+        //     })
+        //     .then(() => {
+        //       setRedirect(true);
+        //     })
+        //     .catch((err) => {
+        //       console.log(err);
+        //     });
+      }
+    },
+  },
+};
 </script>
 
 <style scoped>
-.container{
+.container {
   overflow: hidden;
 }
-.card{
+.card {
   position: absolute;
   margin: 15% 8%;
 }
-.content{
+.content {
   display: flex;
   flex-direction: column;
   float: right;
@@ -83,26 +182,57 @@ export default {
   margin-right: 20px;
   line-height: 40px;
 }
-.content label{
+.content label {
   text-align: left;
-  color: #C6C6C6;
-  font-family: 'Segoe UI', sans-serif;
+  color: #c6c6c6;
+  font-family: "Segoe UI", sans-serif;
 }
-.content input{
+.content input,
+select {
   height: 30px;
   border: none;
-  border-bottom: 1px solid #C6C6C6;
+  border-bottom: 1px solid #c6c6c6;
   outline: none;
+  color: #707070;
 }
-.cardInformation{
+.cardInformation {
   display: flex;
   width: 100%;
   justify-content: space-between;
 }
-.cardInformation label{
+.cardInformation label {
   width: 25%;
+  margin-bottom: 10px;
 }
-.cardInformation input{
+.cardInformation input {
   width: 100%;
+  color: #707070;
+}
+span {
+  color: #ff0000;
+  font-size: 12px;
+  text-align: left;
+  line-height: 15px;
+}
+.cardInformation label span {
+  position: absolute;
+}
+img {
+  width: 8%;
+  cursor: pointer;
+}
+button {
+  background-color: #01ee55;
+  border: none;
+  cursor: pointer;
+  color: #ffffff;
+  font-size: 18px;
+  width: 150px;
+  height: 60px;
+  border-radius: 10px;
+  margin: 5% 0 5% auto;
+}
+button:hover {
+  box-shadow: 1px 5px 10px #c6c6c6;
 }
 </style>
