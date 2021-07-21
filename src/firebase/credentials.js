3863f9c7065517a325c8a5395a69545558144358
sulamita_althaus <sulamitaalthaus44@gmail.com>
2021-07-21 16:36:36 -0300

    adjusted readme

---
+++ src/firebase/credentials.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyC7aevM2wFG2_2btjNSZNgfx6Ip11SzULA",
+    authDomain: "eureciclo-9428d.firebaseapp.com",
+    projectId: "eureciclo-9428d",
+    storageBucket: "eureciclo-9428d.appspot.com",
+    messagingSenderId: "263914822507",
+    appId: "1:263914822507:web:fb5c83f7c2aebf1ece8a2e",
+    measurementId: "G-13FN8BRN7X"
+  };
